3aaead146ea73b0d8a825165e425e994a97abdbc
Hebert <hebert50vitor@hotmail.com>
2023-12-29 14:21:50 -0300

    content page created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Anunciado filme dos Guardiões da Galáxia',
+    description:
+      'Novo lançamento surpreende por seus personagens fora do mainstream',
+    photoCover:
+      'https://th.bing.com/th/id/OIP.whbhQ4JYOIz-WvVS7tIXFAHaEK?w=333&h=187&c=7&r=0&o=5&pid=1.7',
+  },
+
+  {
+    id: '2',
+    title: 'Nova serie anunciada!',
+    description: 'Mais conteúdo para enriquecer a plataforma',
+    photoCover:
+      'https://th.bing.com/th/id/OIP.cv4dS8F2MdKLN32EICzbxQHaEM?w=258&h=180&c=7&r=0&o=5&pid=1.7',
+  },
+  {
+    id: '3',
+    title: 'Nova serie anunciada!',
+    description: 'Mais conteúdo para enriquecer a plataforma',
+    photoCover:
+      'https://th.bing.com/th/id/OIP.cv4dS8F2MdKLN32EICzbxQHaEM?w=258&h=180&c=7&r=0&o=5&pid=1.7',
+  },
+  {
+    id: '4',
+    title: 'Curiosidades sobre o mundo das HQs',
+    description: 'A jornada historica dessa mídia',
+    photoCover:
+      'https://th.bing.com/th/id/OIP.mACjX7PoT9EKq3TdnURFYgHaEO?w=279&h=180&c=7&r=0&o=5&pid=1.7',
+  },
+];
